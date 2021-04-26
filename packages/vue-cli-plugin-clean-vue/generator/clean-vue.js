62469a36d79117ffbbdfcd4353a5bf1c2f62d344
soluteli <soluteli@163.com>
2021-04-26 16:44:23 +0800

    chore

--- packages/vue-cli-plugin-clean-vue/generator/clean-vue.js
+++ packages/vue-cli-plugin-clean-vue/generator/clean-vue.js
@@ -1,10 +1,6 @@
 const { loadModule } = require('@vue/cli-shared-utils')
 module.exports = async (api, options) => {
 
-  const fs = loadModule('fs-extra', api.generator.context)
-  fs.emptyDirSync(api.resolve('./src'))
-  fs.emptyDirSync(api.resolve('./public'))
-
   // 删除项目初始文件 https://github.com/vuejs/vue-cli/blob/7f56846a2e54ee5b748232e8b5ae2411a147416d/packages/@vue/cli/lib/Generator.js#L173
   Object.keys(api.generator.files).forEach(p => {
     const _path = p.replace(/\\/g, '/')
@@ -12,11 +8,16 @@ module.exports = async (api, options) => {
       Reflect.deleteProperty(api.generator.files, _path)
     }
   })
-
   // 2. 删除 package.json vue 项目依赖
   for (const k of ['vue-template-compiler', 'vue']) {
-    if (api.generator.pkg.devDependencies[k]) {
+    if (api.generator.pkg.devDependencies[k] || api.generator.pkg.dependencies[k]) {
       await api.generator.pm.remove(k)
     }
   }
+
+  api.afterInvoke(() => {
+    const fs = loadModule('fs-extra', api.generator.context)
+    fs.emptyDirSync(api.resolve('./src'))
+    fs.emptyDirSync(api.resolve('./public'))
+  })
 }
