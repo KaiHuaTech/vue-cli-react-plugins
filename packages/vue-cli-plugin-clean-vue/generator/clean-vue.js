ba1ed9b8dec7b1a06febbe54d33fbdeeeace21ea
soluteli <soluteli@163.com>
2021-04-26 20:06:15 +0800

    fix: 尝试解决不删除文件的问题

--- packages/vue-cli-plugin-clean-vue/generator/clean-vue.js
+++ packages/vue-cli-plugin-clean-vue/generator/clean-vue.js
@@ -2,23 +2,23 @@ const { loadModule } = require('@vue/cli-shared-utils')
 module.exports = async (api, options) => {
 
   // 删除项目初始文件 https://github.com/vuejs/vue-cli/blob/7f56846a2e54ee5b748232e8b5ae2411a147416d/packages/@vue/cli/lib/Generator.js#L173
-  Object.keys(api.generator.files).forEach(p => {
+  /* Object.keys(api.generator.files).forEach(p => {
     const _path = p.replace(/\\/g, '/')
     if (_path.startsWith('src') || _path.endsWith('public')) {
       Reflect.deleteProperty(api.generator.files, _path)
     }
-  })
+  }) */
 
-  api.afterInvoke(() => {
-    const fs = loadModule('fs-extra', api.generator.context)
-    fs.emptyDirSync(api.resolve('./src'))
-    fs.emptyDirSync(api.resolve('./public'))
+  const fs = loadModule('fs-extra', api.generator.context)
+  fs.emptyDirSync(api.resolve('./src'))
+  fs.emptyDirSync(api.resolve('./public'))
 
-    // 2. 删除 package.json vue 项目依赖
-    for (const k of ['vue-template-compiler', 'vue']) {
-      if (api.generator.pkg.devDependencies[k] || api.generator.pkg.dependencies[k]) {
-        api.generator.pm.remove(k)
-      }
+  // 2. 删除 package.json vue 项目依赖
+  for (const k of ['vue-template-compiler', 'vue']) {
+    if (api.generator.pkg.devDependencies[k] || api.generator.pkg.dependencies[k]) {
+      await api.generator.pm.remove(k)
     }
-  })
+  }
+  // api.afterInvoke(() => {
+  // })
 }
--- packages/vue-cli-plugin-clean-vue/generator/index.js
+++ packages/vue-cli-plugin-clean-vue/generator/index.js
@@ -1,7 +1,7 @@
 const cleanVue = require('./clean-vue')
 
-module.exports = (...args) => {
+module.exports = async (...args) => {
   const [api, options] = args
   // 删除项目文件
-  cleanVue(api, options)
+  await cleanVue(api, options)
 }
