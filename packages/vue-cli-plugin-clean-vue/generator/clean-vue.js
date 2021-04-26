9d1bf7c9703463d807d72e6b791bd5b0f646a6ed
soluteli <soluteli@163.com>
2021-04-26 16:24:25 +0800

    fix: 修复 clean-vue 的问题

--- packages/vue-cli-plugin-clean-vue/generator/clean-vue.js
+++ packages/vue-cli-plugin-clean-vue/generator/clean-vue.js
@@ -1,10 +1,17 @@
-const { loadModule } = require('@vue/cli-shared-utils')
-module.exports = (api, options) => {
-  // 1. 删除原项目 vue 项目 src、public 目录文件
-  const fs = loadModule('fs-extra', api.generator.context)
-  fs.emptyDirSync(api.resolve('./src'))
-  fs.emptyDirSync(api.resolve('./public'))
+module.exports = async (api, options) => {
+
+  // 删除项目初始文件 https://github.com/vuejs/vue-cli/blob/7f56846a2e54ee5b748232e8b5ae2411a147416d/packages/@vue/cli/lib/Generator.js#L173
+  Object.keys(api.generator.files).forEach(p => {
+    const _path = p.replace(/\\/g, '/')
+    if (_path.startsWith('src') || _path.endsWith('public')) {
+      Reflect.deleteProperty(api.generator.files, _path)
+    }
+  })
 
   // 2. 删除 package.json vue 项目依赖
-  api.generator.pm.remove('vue')
+  for (const k of ['vue-template-compiler', 'vue']) {
+    if (api.generator.pkg.devDependencies[k]) {
+      await api.generator.pm.remove(k)
+    }
+  }
 }
