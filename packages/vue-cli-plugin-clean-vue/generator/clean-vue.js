a5c5c09f3653e860bf4383deccf751f87662a34a
soluteli <soluteli@163.com>
2021-04-26 16:35:00 +0800

    revoke: 恢复删除文件的代码

--- packages/vue-cli-plugin-clean-vue/generator/clean-vue.js
+++ packages/vue-cli-plugin-clean-vue/generator/clean-vue.js
@@ -1,4 +1,9 @@
+const { loadModule } = require('@vue/cli-shared-utils')
 module.exports = async (api, options) => {
+
+  const fs = loadModule('fs-extra', api.generator.context)
+  fs.emptyDirSync(api.resolve('./src'))
+  fs.emptyDirSync(api.resolve('./public'))
 
   // 删除项目初始文件 https://github.com/vuejs/vue-cli/blob/7f56846a2e54ee5b748232e8b5ae2411a147416d/packages/@vue/cli/lib/Generator.js#L173
   Object.keys(api.generator.files).forEach(p => {
